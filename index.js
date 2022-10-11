90bc88c517da940c94722e553afad84f3fd3240b
FruityKitKats <dragonminner867@gmail.com>
2022-10-11 11:10:30 +0200

    remove unused functions

--- index.js
+++ index.js
@@ -1,29 +1,24 @@
-// Copyright (c) 2020 FruityKitKats. All rights reserved.
+// Copyright (c) 2017 FruityKitKats. All rights reserved.
 
 // Packages:
-const Discord = require("discord.js"); // `npm i discord.js`
+const Discord = require('discord.js'); // `npm i discord.js`
 const bot = new Discord.Client();
-const weather = require("weather-js"); // `npm i weather-js`
-const fs = require("fs"); // No need to install this, it's already included within node.
+
+// .env file
+const { PREFIX, TOKEN } = process.env;
 
 // Functions
-const func = require("./functions.js"); 
-
-// JSON
-const commands = JSON.parse(fs.readFileSync("Storage/commands.json", "utf8"));
+const func = require('./functions.js'); 
 
 // Global setings 
-const prefix = process.env.PREFIX; 
+const prefix = PREFIX; 
 
 // Listener Event: Runs whenever a message is received.
-bot.on("message", message => {
-
-    // Variables 
-    let msg = message.content.toUpperCase(); 
+bot.on('message', message => {
+    // Variables  
     let sender = message.author; 
     let args = message.content.slice(prefix.length).trim().split(" "); 
     let cmd = args.shift().toLowerCase(); 
-
     if (sender.bot) return;
     if (!message.content.startsWith(prefix)) return; 
 
@@ -32,20 +27,15 @@ bot.on("message", message => {
         let commandFile = require(`./commands/${cmd}.js`);
         commandFile.run(bot, message, args, func); 
     } catch(e) { 
-
         console.log(e.message); 
-
     } finally { 
-
         console.log(`${message.author.username} ran the command: ${cmd}`); 
-
     }  
-
 });
 
 // Listener Event: Runs whenever the bot sends a ready event 
-bot.on("ready", () => {
-    console.log("Bot started.")
+bot.on('ready', () => {
+    console.log('Bot started.');
 });
 
-bot.login(process.env.TOKEN);
+bot.login(TOKEN);
